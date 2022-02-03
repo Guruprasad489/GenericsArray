df13227d2200dafe7cb4c839b1d9499157c2af25
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-02-03 18:56:24 +0530

    [Guru] Welcome to Print Array Program using Generics

---
+++ GenericsArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics Concept\n");
+
+
+        }
+    }
+}
